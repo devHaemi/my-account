f44ae1db3071b313abbd9ffc9906dde33bd97e58
hmJeon <hmjeon@aiblab.co.kr>
2025-01-03 11:40:19 +0900

    이벤트 배너 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
+import dynamic from 'next/dynamic'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
+
+import Skeleton from '@components/shared/Skeleton'
+
+const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
+  ssr: false,
+  loading: () => (
+    <Skeleton width="100%" height={100} style={{ borderRadius: 8 }}>
+      loading...
+    </Skeleton>
+  ),
+})
 
 export default function Home() {
   return (
     <Container>
+      <EventBanners />
       <div css={bold}>Home</div>
     </Container>
   )

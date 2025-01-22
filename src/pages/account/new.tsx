5d26c09cd2369a3314f7d75929a6633021be96cc
hmJeon <hmjeon@aiblab.co.kr>
2025-01-22 15:29:25 +0900

    계좌개설

--- src/pages/account/new.tsx
+++ src/pages/account/new.tsx
@@ -1,27 +1,35 @@
 import { useState } from 'react'
 import { getSession } from 'next-auth/react'
-
-import Terms from '@components/account/Terms'
-import ProgressBar from '@components/shared/ProgressBar'
-import useUser from '@hooks/useUser'
-import { getTerms, setTerms } from '@remote/account'
+import dynamic from 'next/dynamic'
 import { GetServerSidePropsContext } from 'next'
+import { useRouter } from 'next/router'
+
+import withAuth from '@hooks/withAuth'
+import ProgressBar from '@shared/ProgressBar'
+import Terms from '@components/account/Terms'
+import useUser from '@hooks/useUser'
+import { getTerms, setTerms, createAccount, getAccount } from '@remote/account'
 import { User } from '@models/user'
+import Form from '@components/account/Form'
+import { Account } from '@models/account'
+import FullPageLoader from '@components/shared/FullPageLoader'
+
+const FixedBottomButton = dynamic(() => import('@shared/FixedBottomButton'))
 
 // STEP 0 = 약관동의
-// STEP 1 = 계좌개설 폼
-// STEP 2 = 완료
-const LAST_STEP = 2
+// STEP 1 = 계좌 개설 폼 페이지
+// STEP 2 = 완료페이지
+const LAST_STEP = 2 // 완료페이지
 
 function AccountNew({ initialStep }: { initialStep: number }) {
-  const [step, setStep] = useState(0)
+  const [step, setStep] = useState(initialStep)
   const user = useUser()
-
-  console.log(initialStep)
+  const navigate = useRouter()
 
   return (
     <div>
       <ProgressBar progress={step / LAST_STEP} />
+
       {step === 0 ? (
         <Terms
           onNext={async (termIds) => {
@@ -30,6 +38,36 @@ function AccountNew({ initialStep }: { initialStep: number }) {
             setStep(step + 1)
           }}
         />
+      ) : null}
+
+      {step === 1 ? (
+        <Form
+          onNext={async (formValues) => {
+            const newAccount = {
+              ...formValues,
+              accountNumber: Date.now(),
+              balance: 0,
+              status: 'READY',
+              userId: user?.id as string,
+            } as Account
+
+            await createAccount(newAccount)
+
+            setStep(step + 1)
+          }}
+        />
+      ) : null}
+
+      {step === 2 ? (
+        <>
+          <FullPageLoader message="계좌개설 신청이 완료되었어요" />
+          <FixedBottomButton
+            label="확인"
+            onClick={() => {
+              navigate.push('/')
+            }}
+          />
+        </>
       ) : null}
     </div>
   )
@@ -48,7 +86,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
   }
 
-  if (agreedTerms != null) {
+  const account = await getAccount((session?.user as User).id)
+
+  if (account == null) {
     return {
       props: {
         initialStep: 1,
@@ -58,9 +98,9 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   return {
     props: {
-      initialStep: 0,
+      initialStep: 2,
     },
   }
 }
 
-export default AccountNew
+export default withAuth(AccountNew)
